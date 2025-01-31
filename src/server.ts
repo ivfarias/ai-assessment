ed3e9f3c45d95f05d450cd138541d3bdcc76692b
Carlos André <carlos@kyte.com.br>
2025-01-31 16:26:02 -0300

    feat: install and configure mongo in the project

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,68 @@
+import 'module-alias/register';
 import fastify from 'fastify';
 import fastifyCookie from '@fastify/cookie';
 import fastifyHelmet from '@fastify/helmet';
 import cors from '@fastify/cors';
 import dotenv from 'dotenv';
-import indexRouter from './routes/index.js';
-import { logger } from './middleware/logger.js';
+import indexRouter from '@/routes/index.js';
+import { logger } from '@/middleware/logger.js';
+import db from '@/config/mongodb.js';
 
 dotenv.config();
 
-const app = fastify();
-
-app.addHook('onRequest', logger);
-
-// Middleware
-app.register(fastifyCookie);
-app.register(fastifyHelmet);
-app.register(cors);
-
-// Routes
-app.register(indexRouter);
-
-// Catch 404 and forward to error handler
-app.setNotFoundHandler((_, reply) => {
-  reply.status(404).send({ error: 'Not Found' });
+const app = fastify({
+  logger: true, // Habilita o logger do Fastify
 });
 
-// Error handler
-app.setErrorHandler((error, request, reply) => {
-  // Log the error with more details
-  console.error('Error details:', {
-    message: error.message,
-    stack: error.stack,
-    url: request.url,
-    method: request.method,
-    body: request.body,
-  });
+const start = async () => {
+  try {
+    // Database
+    await app.register(db);
 
-  // Send error response
-  reply.status(error.statusCode || 500).send({
-    message: error.message,
-    error: process.env.NODE_ENV === 'development' ? error : {},
-  });
-});
+    // plugins
+    await app.register(fastifyCookie);
+    await app.register(fastifyHelmet);
+    await app.register(cors);
 
-// For Vercel serverless deployment and local development
+    // Middlewares
+    app.addHook('onRequest', logger);
+
+    // Routes
+    await app.register(indexRouter);
+
+    // Catch 404 and forward to error handler
+    app.setNotFoundHandler((_, reply) => {
+      reply.status(404).send({ error: 'Not Found' });
+    });
+
+    // Error handler
+    app.setErrorHandler((error, request, reply) => {
+      console.error('Error details:', {
+        message: error.message,
+        stack: error.stack,
+        url: request.url,
+        method: request.method,
+        body: request.body,
+      });
+
+      // Send error response
+      reply.status(error.statusCode || 500).send({
+        message: error.message,
+        error: process.env.NODE_ENV === 'development' ? error : {},
+      });
+    });
+
+    if (process.env.NODE_ENV !== 'production') {
+      const PORT = Number(process.env.PORT) || 3000;
+      await app.listen({ port: PORT });
+      console.log(`Server is running on port ${PORT}`);
+    }
+  } catch (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1);
+  }
+};
+
+start();
+
 export default app;
-
-// For local development server
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = Number(process.env.PORT) || 3000;
-  console.log(`Starting server on port ${PORT}`);
-  app.listen({
-    port: PORT,
-  });
-}
---
+++ src/util/logger.ts
@@ -0,0 +1,36 @@
+import LogdnaWinston from 'logdna-winston';
+import winston from 'winston';
+
+const key = process.env.LOGDNA_KEY;
+
+const logger = winston.createLogger({
+  transports: [
+    new LogdnaWinston({
+      key,
+      app: 'kyte-ai',
+      env: process.env.NODE_ENV,
+      level: 'debug',
+      indexMeta: true,
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    }),
+  );
+}
+
+export function info(message: string, meta?: any) {
+  return logger.info(message, meta);
+}
+
+export function warn(message: string) {
+  return logger.warn(message);
+}
+
+export function error(message: string, meta?: any, callback?: () => void) {
+  return logger.error(message, meta, callback);
+}
